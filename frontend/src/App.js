c6430cc1cba0d9381069e17e1d73da978f09e4de
SRIVATSAN S <95332677+vatsan-0613@users.noreply.github.com>
2024-03-15 00:13:18 +0530

    Added OTP Form Page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,15 @@
 
 import './App.css';
 import Patientprofiles from "./components/Patientprofiles";
+import Otp from './components/Otp';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 
 function App() {
   return (
     <BrowserRouter>
         <Routes>
+          <Route path="/otp" element={<Otp />} />
           <Route path="/patientprofile" element={<Patientprofiles />} />
         </Routes>
       </BrowserRouter>
